620ed764973d8cf6304929475512a876660ca264
ZumeShougo <g0109725rena@gmail.com>
2023-02-16 13:16:31 +0900

    import react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import SignUp from './components/SignUp';
 import { AuthProvider } from './context/authContext.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <AuthProvider>
       <div style={{ margin: '2em' }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/signup' element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
         <SignUp />
       </div>
     </AuthProvider>
