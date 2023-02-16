fd9e264b9c09ee3c5c21ee2a2fb6c567c0eb5ce6
ZumeShougo <g0109725rena@gmail.com>
2023-02-16 16:27:06 +0900

    Add redirect from /login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,41 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
 
 
 const Login = () => {
-  const handleSubmit = (e) => {
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const {email, password} = e.target.elements;
-    signInWithEmailAndPassword(auth, email.value, password.value);
+    signInWithEmailAndPassword(auth, email.value, password.value)
+      .then(() => {
+        navigate('/');
+      })
+      .catch((error) => {
+        switch(error.code){
+          case 'auth/invalid-email':
+            setError('正しいメールアドレス形式で入力してください。');
+            break;
+          case 'auth/user-not-found':
+            setError('メールアドレスかパスワードに誤りがあります。');
+            break;
+          case 'auth/wrong-password':
+            setError('メールアドレスかパスワードに誤りがあります。');
+            break;
+          default:
+            setError('メールアドレスかパスワードに誤りがあります。')
+            break;
+        }
+      });
   };
   return (
     <div>
       <h1>ログイン画面</h1>
       <form onSubmit={handleSubmit}>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
         <div>
           <label htmlFor="email">メールアドレス</label>
           <input id="email" name="email" type="email" placeholder="メールアドレス" />
