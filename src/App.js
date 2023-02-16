c3734c6c45b81b182a7d3c5949b55f2f89f2277d
ZumeShougo <g0109725rena@gmail.com>
2023-02-16 13:23:16 +0900

    Add routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import SignUp from './components/SignUp';
 import { AuthProvider } from './context/authContext.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -10,9 +12,10 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path='/signup' element={<SignUp />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
           </Routes>
         </BrowserRouter>
-        <SignUp />
       </div>
     </AuthProvider>
   );
